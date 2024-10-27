844ba061ce49367a12fc0481c55b2d4c4e8543f2
Dqrk <70263848+Dqrkky@users.noreply.github.com>
2024-10-27 19:05:33 +0200

    update Printer.py

--- Printer.py
+++ Printer.py
@@ -1,71 +1,24 @@
-import requests, json, xmltodict, base64
-
-def req(js0n: dict=None, r=None):
-    if js0n != None and isinstance(js0n, dict) and len(js0n) > 0:
-        if "r" in js0n and js0n["r"] != None:
-            r_ = js0n["r"]
-        else:
-            if r != None:
-                r_ = r
-            else:
-                r_ = None
-        if "method" in js0n and js0n["method"] != None:
-            method_ = js0n["method"]
-        else:
-            method_ = None
-        if "url" in js0n and js0n["url"] != None:
-            url_ = js0n["url"]
-        else:
-            url_ = None
-        if "params" in js0n and js0n["params"] != None:
-            params_ = js0n["params"]
-        else:
-            params_ = None
-        if "data" in js0n and js0n["data"] != None:
-            data_ = js0n["data"]
-        else:
-            data_ = None
-        if "headers" in js0n and js0n["headers"] != None:
-            headers_ = js0n["headers"]
-        else:
-            headers_ = None
-        if "aftermethod" in js0n and js0n["aftermethod"] != None:
-            aftermethod_ = js0n["aftermethod"]
-        else:
-            aftermethod_ = None
-        if r_ != None and method_ != None and url_ != None and aftermethod_ != None:
-            re = r_.request(
-                method=method_,
-                url=url_,
-                params=params_,
-                data=data_,
-                headers=headers_
-            )
-            if aftermethod_ == "text":
-                data_out = re.text
-            elif aftermethod_ == "json":
-                data_out = re.json()
-            elif aftermethod_ == "content":
-                data_out = re.content
-            elif aftermethod_ == "re":
-                data_out = re
-            else:
-                data_out = None
-            return data_out
+import requests
+import json
+import xmltodict
+import base64
 
 class HP044C0C:
     def __init__(self, host: str="http://hp044c0c", username :str="admin", password :str=None):
         with requests.Session() as rss:
             self.rss = rss
-        self.rss.headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.52"
+        self.shared = shared.Shared(
+            rss=self.rss
+        )
         self.config = {
             "host": host,
             "headers": {
-                "Authorization": None
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.52"
             }
         }
         if username != None and isinstance(username, str) and password != None and isinstance(password, str):
             self.config["headers"]["Authorization"] = f"Basic {base64.b64encode(f'{username}:{password}'.encode()).decode()}"
+        self.rss.headers.update(self.config["headers"])
     def authcheck(self):
         #https://192.168.1.3/AuthChk
         pass
