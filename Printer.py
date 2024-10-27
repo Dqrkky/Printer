80b8df2564dbf6bb1dc5f5a81812485c8ae3439a
Dqrk <70263848+Dqrkky@users.noreply.github.com>
2024-10-27 19:14:15 +0200

    update Printer.py

--- Printer.py
+++ Printer.py
@@ -118,6 +118,9 @@ class Scan:
     def __init__(self):
         with requests.Session() as rss:
             self.rss = rss
+        self.shared = shared.Shared(
+            rss=self.rss
+        )
         self.config = {
             "headers": {
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.52"
