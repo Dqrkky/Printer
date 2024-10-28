ca4fb3a2328214a889149ea2041ebba088e38346
Dqrk <70263848+Dqrkky@users.noreply.github.com>
2024-10-28 12:33:45 +0200

    update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import Printer
+import printer
 
-hpenvy6020e = Printer.HP044C0C()
-scann = Printer.Scan()
+hpenvy6020e = printer.HP044C0C()
+#scann = Printer.Scan()
 
 print(hpenvy6020e.getconfigurationconstraints())
